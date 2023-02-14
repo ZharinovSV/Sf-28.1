a389064ba895479b5441914b547bef368d76d913
ZharinovSV <Zharinov.serg@yandex.ru>
2023-02-14 16:54:08 +0300

    new file

---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "/tests/chromedriver.exe"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'Ввести валидный email'       # Ввести валидный email
+    valid_password = 'Ввести валидный пароль'   # Ввести валидный пароль
+    valid_email_reg = 'Ввести валидный email'   # Ввести валидный email
+    invalid_email = 'ya.yy.com'
+    invalid_password = 'invalid15464'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Бруно'
+    last_name = 'Усатович'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'г'
+    latin_generate_string = 'aattoy'
+    chinese_chars = '形声字/形聲字象形字'
+    special_chars = '!@#$%^&*()_+=/{}'
+    numbers = 351
+    empty = ''
+    passw1 = 'QwE12345'
+    passw2 = 'Zwa987456vBn09Ds'
+    passw3 = 'WaSwsX1987654321Ujb30'
